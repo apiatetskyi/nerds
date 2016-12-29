86c9846c7f652a0bfa5b18fe6d5b27833bd3fd4f
Aleksei Piatetskyi <a.piatetskyi@gmail.com>
2016-12-29 20:30:38 +0200

    Finished map block

--- js/app.js
+++ js/app.js
@@ -6,3 +6,22 @@ $(document).ready(function(){
     dots: true
   });
 });
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 59.939132, lng: 30.321471},
+    zoom: 17,
+    scaleControl: true,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false
+  });
+
+  var image = 'img/map-marker.png';
+  var nerdsMarker = new google.maps.Marker({
+    position: {lat: 59.938752, lng: 30.323789},
+    map: map,
+    icon: image
+  });
+}
